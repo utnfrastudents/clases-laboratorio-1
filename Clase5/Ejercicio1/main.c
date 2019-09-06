2802e77e91307093fd94eb3d88d917da1f7eaafe
Christian Grimberg <christian.grimberg@outlook.com>
2019-09-06 19:29:30 -0300

    :+1: Prueba de cargar y mostrar el contenido de un vector/array.

--- Clase5/Ejercicio1/main.c
+++ Clase5/Ejercicio1/main.c
@@ -7,7 +7,18 @@ int main()
     int numeros[5] = {5, 3, 4, 1, 9}; //Declaracion explicita
     int inicializacionEnCero[5] = {0}; //Inicializar todos sus elementos en cero
 
-    printf("%d\n%d\n%d\n", x, numeros[1], inicializacionEnCero[1]);
+    printf("%d\n%d\n%d\n%d\n", x, numeros[1], inicializacionEnCero[1], *numeros);
+
+    for(int j = 0; j < 5; j++)
+    {
+        printf("Ingrese un numero para la posicion %d: ", j);
+        scanf("%d", &numeros[j]);
+    }
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("El valor del indice %d es %d\n", i, numeros[i]);
+    }
 
     return 0;
 }
