bfe9d03600a2b672217b4734d662902159546eeb
alumno <alumno@LAB00PC00>
2019-09-09 22:07:03 -0300

    :+1: Hardcodeado de la carga de alumnos y encapsulado a funcion para mostrar un alumno.

--- Clase06/Ejercicio3/main.c
+++ Clase06/Ejercicio3/main.c
@@ -1,41 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAM 2
+#define TAM 5
 
+void mostrarAlumno(int leg, int edad, char sex, int nP1, int nP2, float prom);
 void mostrarAlumnos(int leg[], int edad[], char sex[], int nP1[], int nP2[], float prom[], int tam);
 void clearBuffer(void);
 
 int main()
 {
-    int legajos[TAM];
-    int edades[TAM];
-    char sexos[TAM];
-    int notasP1[TAM];
-    int notasP2[TAM];
-    float promedios[TAM];
+    int legajos[TAM] = {1234, 1122, 2233, 3344, 4455};
+    int edades[TAM] = {22, 33, 44, 55, 66};
+    char sexos[TAM] = {'f', 'f', 'm', 'f', 'm'};
+    int notasP1[TAM] = {8, 2, 9, 8, 5};
+    int notasP2[TAM] = {2, 5, 8, 7, 4};
+    float promedios[TAM] = {3.0, 5.6, 7.8, 8.7, 5.2};
 
-    for(int i=0; i < TAM; i++)
-    {
-        printf("\nIngreso de datos\n");
-        printf("Legajo: ");
-        scanf("%d", &legajos[i]);
-        printf("Edad: ");
-        scanf("%d", &edades[i]);
-        printf("Sexo: ");
-        clearBuffer();
-        scanf("%c", &sexos[i]);
-        printf("Parcial 1: ");
-        scanf("%d", &notasP1[i]);
-        printf("Parcial 2: ");
-        scanf("%d", &notasP2[i]);
-
-        promedios[i] = (float)(notasP1[i] + notasP2[i])/2;
-    }
+//    for(int i=0; i < TAM; i++)
+//    {
+//        printf("\nIngreso de datos\n");
+//        printf("Legajo: ");
+//        scanf("%d", &legajos[i]);
+//        printf("Edad: ");
+//        scanf("%d", &edades[i]);
+//        printf("Sexo: ");
+//        clearBuffer();
+//        scanf("%c", &sexos[i]);
+//        printf("Parcial 1: ");
+//        scanf("%d", &notasP1[i]);
+//        printf("Parcial 2: ");
+//        scanf("%d", &notasP2[i]);
+//
+//        promedios[i] = (float)(notasP1[i] + notasP2[i])/2;
+//    }
 
     mostrarAlumnos(legajos, edades, sexos, notasP1, notasP2, promedios, TAM);
 
     return 0;
+}
+
+void mostrarAlumno(int leg, int edad, char sex, int nP1, int nP2, float prom)
+{
+    printf("    %d       %d      %c       %d      %d      %.2f\n",
+        leg, edad, sex, nP1, nP2, prom);
 }
 
 void mostrarAlumnos(int leg[], int edad[], char sex[], int nP1[], int nP2[], float prom[], int tam)
@@ -47,8 +54,7 @@ void mostrarAlumnos(int leg[], int edad[], char sex[], int nP1[], int nP2[], flo
 
         for(int i=0; i < tam; i++)
         {
-            printf("    %d       %d      %c       %d      %d      %.2f\n",
-                leg[i], edad[i], sex[i], nP1[i], nP2[i], prom[i]);
+            mostrarAlumno(leg[i], edad[i], sex[i], nP1[i], nP2[i], prom[i]);
         }
     }
 }
