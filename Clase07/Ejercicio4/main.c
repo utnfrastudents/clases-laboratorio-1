e883c221b2c17c61d25256729959a3065b523233
Christian Grimberg <christian.grimberg@outlook.com>
2019-09-16 18:11:09 -0300

    :+1: Funcion para imprimir los nombres cargados en una matriz de vectores.

--- Clase07/Ejercicio4/main.c
+++ Clase07/Ejercicio4/main.c
@@ -2,11 +2,13 @@
 
 #define CANTIDAD_NOMBRES 5
 #define NOMBRE_LONGITUD 20
-#define UNA_LINEA 1
+#define UNA_LINEA 1 /**< Debe tomar los valores [1] indicando una linea [0] todas las lineas. >*/
 
-typedef char mNombres[CANTIDAD_NOMBRES][NOMBRE_LONGITUD];
+typedef char tNombre[NOMBRE_LONGITUD];
+typedef tNombre mNombres[CANTIDAD_NOMBRES];
 
-void cargarNombres(mNombres nombres, int cantidad, int longitud);
+void cargarNombres(mNombres* nombres, int cantidad, int longitud);
+void imprimirNombres(mNombres* nombres, int cantidad, int longitud);
 
 int main()
 {
@@ -14,14 +16,15 @@ int main()
 
     mNombres nombres;
 
-    cargarNombres(nombres, CANTIDAD_NOMBRES, NOMBRE_LONGITUD);
+    cargarNombres(&nombres, CANTIDAD_NOMBRES, NOMBRE_LONGITUD);
+    imprimirNombres(&nombres, CANTIDAD_NOMBRES, NOMBRE_LONGITUD);
 
     input_pauseScreen("Presione Enter para salir...");
 
     return 0;
 }
 
-void cargarNombres(mNombres nombres, int cantidad, int longitud)
+void cargarNombres(mNombres* nombres, int cantidad, int longitud)
 {
     int longitudAux;
 
@@ -36,17 +39,46 @@ void cargarNombres(mNombres nombres, int cantidad, int longitud)
         {
             longitudAux = longitud;
         }
-        
-        
+
         for(int i = 0; i < cantidad; i++)
         {
             for(int j = 0; j < longitudAux; j++)
             {
-                if(!input_getString(&nombres[i][j], "Ingrese un nombre: ", "Intente nuevamente: ", 1, longitud))
+                if(!input_getString(nombres[i][j], "Ingrese un nombre: ", "Intente nuevamente: ", 1, longitud))
                 {
-                    printf("Nombre %d ingresado correctamente: %s\n", i+1, &nombres[i][j]);
+                    printf("Nombre %d ingresado correctamente: %s\n", i+1, nombres[i][j]);
                 }
             }
         }
+    }
+}
+
+void imprimirNombres(mNombres* nombres, int cantidad, int longitud)
+{
+    int longitudAux;
+
+    if(nombres != NULL && cantidad > 0 && cantidad <= CANTIDAD_NOMBRES
+        && longitud > 0 && longitud <= NOMBRE_LONGITUD)
+    {
+        if(UNA_LINEA)
+        {
+            longitudAux = 1;
+        }
+        else
+        {
+            longitudAux = longitud;
+        }
+
+        printf("\n%s\n", "=======================");
+        printf("%s\n", "        NOMBRES        ");
+        printf("%s\n", "=======================");
+        for(int i = 0; i < cantidad; i++)
+        {
+            for(int j = 0; j < longitudAux; j++)
+            {
+                printf("%s\n", nombres[i][j]);
+            }
+        }
+        printf("%s\n\n", "=======================");
     }
 }
