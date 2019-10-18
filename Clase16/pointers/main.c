f570bd84a818a300e680ebbe70f27eda58b7fa07
Christian Grimberg <christian.grimberg@outlook.com>
2019-10-18 20:41:49 -0300

    :+1: Funcion de intercambio haciendo uso del recurso de los punteros.

--- Clase16/pointers/main.c
+++ Clase16/pointers/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int* p, int* q);
+
 int main()
 {
     /**
@@ -39,5 +41,23 @@ int main()
 
     printf("s: %d\n", *s);
 
+    int x = 5;
+    int y = 10;
+
+    printf("x:%d-y:%d\n", x, y);
+    swap(&x, &y);
+    printf("x:%d-y:%d\n", x, y);
+
     return 0;
+}
+
+void swap(int* p, int* q)
+{
+    int aux;
+
+    aux = *p;
+    *p = *q;
+    *q = aux;
+
+    printf("p:%d-q:%d\n", *p, *q);
 }
