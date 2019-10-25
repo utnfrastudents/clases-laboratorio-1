d28c9c144340c7eced411fbaf5f8c5e26d9ac7bc
Christian Grimberg <christian.grimberg@outlook.com>
2019-10-25 19:21:43 -0300

    :+1: Mostrar los valores de una estructura en funcion de su estado, validando que hay obtenido espacio en memoria previamente.

--- Clase18/practica/main.c
+++ Clase18/practica/main.c
@@ -7,14 +7,20 @@ typedef struct
     int id;
     char nombre[20];
     float sueldo;
-    int estado;
+    int isEmpty;
 }eEmpleado;
 
 eEmpleado* newEmpleado();
+int mostrarEmpleado(eEmpleado* emp);
 
 int main()
 {
     eEmpleado* emp2 = newEmpleado();
+
+    if(!mostrarEmpleado(emp2))
+    {
+        printf("No se puede mostrar el Empleado.\n");
+    }
 
     return 0;
 }
@@ -30,8 +36,21 @@ eEmpleado* newEmpleado()
         aux->id = 0;
         strcpy(aux->nombre, "");
         aux->sueldo = 0;
-        aux->estado = 0;
+        aux->isEmpty = 1;
     }
 
     return aux;
+}
+
+int mostrarEmpleado(eEmpleado* emp)
+{
+    int returnValue = 0;
+
+    if(emp != NULL && !emp->isEmpty)
+    {
+        printf("| %5d | %20s | %5.2f |\n", emp->id, emp->nombre, emp->sueldo);
+        returnValue = 1;
+    }
+
+    return returnValue;
 }
