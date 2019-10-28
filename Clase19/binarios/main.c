432b6497a6bb80d70152d2b5b027d5b243fabf6b
Christian Grimberg <christian.grimberg@outlook.com>
2019-10-28 19:58:11 -0300

    :+1: Leer de archivo binario guardando como cabecera del archivo la longitud del arreglo.

--- Clase19/binarios/main.c
+++ Clase19/binarios/main.c
@@ -13,24 +13,31 @@ void leerBinario(char* path);
 
 int main()
 {
-    escribirBinario("D:\\Clase19\\binarios\\midato.dat");
-    leerBinario("D:\\Clase19\\binarios\\midato.dat");
+    escribirBinario("midato.dat");
+    leerBinario("midato.dat");
 
     return 0;
 }
 
 void escribirBinario(char* path)
 {
-    eEmpleado miEmpleado = {100, "Pepe", 1000};
+    eEmpleado miEmpleado[2] = {{100, "Pepe", 1000}, {101, "Mony", 2000}};
     FILE* miArchivo;
+    int i;
+    int len = 2;
 
     miArchivo = fopen(path, "wb");
 
     if(miArchivo != NULL)
     {
-      if(fwrite((eEmpleado*)(&miEmpleado), sizeof(eEmpleado), 1, miArchivo) == 1)
+      fwrite((int*)&len, sizeof(miEmpleado), 2, miArchivo);
+
+      for(i=0; i<2; i++)
       {
-          printf("Se almaceno el Dato.\n");
+          if(fwrite((eEmpleado*)(&miEmpleado+i), sizeof(eEmpleado), 1, miArchivo) == 1)
+          {
+              printf("Se almacenaron los datos.\n");
+          }
       }
 
       fclose(miArchivo);
@@ -39,16 +46,24 @@ void escribirBinario(char* path)
 
 void leerBinario(char* path)
 {
-    eEmpleado miEmpleado;
+    eEmpleado miEmpleado[10];
     FILE* miArchivo;
+    int i;
+    int len;
 
     miArchivo = fopen(path, "rb");
 
     if(miArchivo != NULL)
     {
-      if(fread((eEmpleado*)(&miEmpleado), sizeof(eEmpleado), 1, miArchivo) == 1)
+      if(fread((int*)&len, sizeof(int), 1, miArchivo) == 1)
       {
-          printf("%d-%s-%.2f.\n", miEmpleado.legajo, miEmpleado.nombre, miEmpleado.salario);
+          if(fread((eEmpleado*)(&miEmpleado), sizeof(eEmpleado), len, miArchivo) == len)
+          {
+              for(i=0; i<len; i++)
+              {
+                  printf("%d-%s-%.2f.\n", miEmpleado[i].legajo, miEmpleado[i].nombre, miEmpleado[i].salario);
+              }
+          }
       }
 
       fclose(miArchivo);
